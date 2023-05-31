2010cf58bda90970bcf1a08973ad53c7ce03acac
Sergio González <thundertechdev@icloud.com>
2023-05-31 21:50:20 +0200

    Initial Commit

---
+++ SimpSynth/SimpSynthApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpSynthApp.swift
+//  SimpSynth
+//
+//  Created by Sergio Gonzalez Cristobal on 31/5/23.
+//
+
+import SwiftUI
+
+@main
+struct SimpSynthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
