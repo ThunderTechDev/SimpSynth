72277ed0efb064946a5399796d6f49771c4a050b
Sergio González <thundertechdev@icloud.com>
2023-06-07 23:27:48 +0200

    This branch will create a Knob struct that will be called from ContentView for represent different knobs, that react to drag gesture responding with a rotation

--- SimpSynth/Views/ContentView.swift
+++ SimpSynth/Views/ContentView.swift
@@ -11,16 +11,27 @@ import Keyboard
 
 struct ContentView: View {
     @StateObject var audioMIDI = AudioMIDI()
-    
+    @State private var rotationKnob1: Double = 0
+    @State private var rotationKnob2: Double = 0
+    @State private var rotationKnob3: Double = 0
+    @State private var rotationKnob4: Double = 0
     
     
     var body: some View {
         ZStack {
             
-            Color.gray
+            Color.black
                 .edgesIgnoringSafeArea(.all)
             
             VStack {
+                
+                HStack {
+                        knobView(title: "Volumen", rotation: $rotationKnob1)
+                        knobView(title: "Release", rotation: $rotationKnob2)
+                        knobView(title: "Reverb", rotation: $rotationKnob3)
+                        knobView(title: "Delay", rotation: $rotationKnob4)
+                    }
+                             
                 
                 Keyboard(layout: .piano(pitchRange: Pitch(60) ... Pitch(72)),
                          noteOn: audioMIDI.noteOn, noteOff: audioMIDI.noteOff) { pitch, isActivated in
@@ -40,7 +51,7 @@ struct ContentView: View {
              
                 
                     
-                    .padding(.all)
+                         
                     .background(.black)
                     
                     .onAppear {
@@ -55,7 +66,7 @@ struct ContentView: View {
                     
                     .environmentObject(audioMIDI)
                     
-                    .padding()
+               
                 
             }
         }
@@ -65,7 +76,25 @@ struct ContentView: View {
        
     }
 
-    
+    func knobView(title: String, rotation: Binding<Double>) -> some View {
+        VStack {
+            Text(title)
+                .font(.body)
+                .foregroundColor(.white)
+
+            Image("Knob")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .rotationEffect(Angle(degrees: rotation.wrappedValue))
+                .gesture(DragGesture(minimumDistance: 0)
+                            .onChanged { gesture in
+                                // Actualizar el valor de la rotación en función del movimiento del arrastre
+                                rotation.wrappedValue = Double(gesture.translation.height)
+                                print(Double(gesture.translation.height))
+                                // Aquí también puedes actualizar el volumen en función de la rotación
+                            })
+        }
+    }
     
        
     
