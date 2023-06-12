515b0ee65291ae317ff0210f707927bb8ac8f6db
Sergio González <thundertechdev@icloud.com>
2023-06-12 23:14:16 +0200

    Limit the rotation of Knobs to 360 degrees. Update label for show score from 0 to 99

--- SimpSynth/Views/ContentView.swift
+++ SimpSynth/Views/ContentView.swift
@@ -11,75 +11,65 @@ import Keyboard
 
 struct ContentView: View {
     @StateObject var audioMIDI = AudioMIDI()
-    
-    
-    
-    var body: some View {
-        
-    
-        
-        
-        ZStack {
-            
-            Color.black
-                .edgesIgnoringSafeArea(.all)
-            
-            VStack {
-                
-                HStack {
-                    KnobView(knobViewModel: KnobViewModel(title: "Volume", audioMIDI: audioMIDI))
-                    KnobView(knobViewModel: KnobViewModel(title: "Reverb", audioMIDI: audioMIDI))
-                    KnobView(knobViewModel: KnobViewModel(title: "Delay", audioMIDI: audioMIDI))
-                    KnobView(knobViewModel: KnobViewModel(title: "Release", audioMIDI: audioMIDI))
-                    }
-                             
-                
-                Keyboard(layout: .piano(pitchRange: Pitch(60) ... Pitch(72)),
-                         noteOn: audioMIDI.noteOn, noteOff: audioMIDI.noteOff) { pitch, isActivated in
-                    KeyboardKey(pitch: pitch,
-                                isActivated: isActivated,
-                                text: "",
-                                pressedColor: Color(.purple),
-                                alignment: .center)
-                    .gesture(DragGesture(minimumDistance: 0)
-                               .onChanged { _ in
-                                   self.audioMIDI.noteOn(pitch: pitch, point: .zero)
-                               }
-                               .onEnded { _ in
-                                   self.audioMIDI.noteOff(pitch: pitch)
-                               })
-                }
-             
-                
-                    
-                         
-                    .background(.black)
-                    
-                    .onAppear {
-                            do {
-                                try audioMIDI.audioEngine.start()
-                            } catch {
-                                print("Error starting audio engine: \(error)")
-                            }
-                        }
-                
-                    
-                    
-                    .environmentObject(audioMIDI)
-                    
-               
-                
-            }
-        }
-        
-       
-        
-       
+
+    // Crea una @StateObject para cada KnobViewModel
+    @StateObject var volumeKnobViewModel: KnobViewModel
+    @StateObject var reverbKnobViewModel: KnobViewModel
+    @StateObject var delayKnobViewModel: KnobViewModel
+    @StateObject var releaseKnobViewModel: KnobViewModel
+
+    init() {
+        let audioMIDI = AudioMIDI()
+        _audioMIDI = StateObject(wrappedValue: audioMIDI)
+        _volumeKnobViewModel = StateObject(wrappedValue: KnobViewModel(title: "Volume", initialValue: 0.0, audioMIDI: audioMIDI))
+        _reverbKnobViewModel = StateObject(wrappedValue: KnobViewModel(title: "Reverb", initialValue: 0.0, audioMIDI: audioMIDI))
+        _delayKnobViewModel = StateObject(wrappedValue: KnobViewModel(title: "Delay", initialValue: 0.0, audioMIDI: audioMIDI))
+        _releaseKnobViewModel = StateObject(wrappedValue: KnobViewModel(title: "Release", initialValue: 0.5, audioMIDI: audioMIDI))
     }
 
-    
-       
-    
+    var body: some View {
+        // Contenido de tu vista
+        ZStack {
+            Color.black.edgesIgnoringSafeArea(.all)
+            VStack {
+                HStack {
+                    KnobView(knobViewModel: volumeKnobViewModel)
+                    KnobView(knobViewModel: reverbKnobViewModel)
+                    KnobView(knobViewModel: delayKnobViewModel)
+                    KnobView(knobViewModel: releaseKnobViewModel)
+                }
+                Keyboard(layout: .piano(pitchRange: Pitch(60) ... Pitch(72)),
+                                      noteOn: audioMIDI.noteOn, noteOff: audioMIDI.noteOff) { pitch, isActivated in
+                                 KeyboardKey(pitch: pitch,
+                                             isActivated: isActivated,
+                                             text: "",
+                                             pressedColor: Color(.purple),
+                                             alignment: .center)
+                                 .gesture(DragGesture(minimumDistance: 0)
+                                            .onChanged { _ in
+                                                self.audioMIDI.noteOn(pitch: pitch, point: .zero)
+                                            }
+                                            .onEnded { _ in
+                                                self.audioMIDI.noteOff(pitch: pitch)
+                                            })
+                             }
+                          
+                             
+                                 
+                                      
+                                 .background(.black)
+                                 
+            }
+        }
+        .onAppear {
+            do {
+                try audioMIDI.audioEngine.start()
+            } catch {
+                print("Error starting audio engine: \(error)")
+            }
+        }
+        .environmentObject(audioMIDI)
+    }
 }
 
 
